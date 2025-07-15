ef4f04f65547b7ee42163a956c1be346db29fadb
adetoadeto <157330742+adetoadeto@users.noreply.github.com>
2025-07-15 21:52:31 +0100

    Expense Tracker f File Uploaded

---
+++ main.js
@@ -0,0 +1,45 @@
+function addMore() {
+    const parentElement = document.querySelector(".expense-list");
+    const form = document.createElement("form");
+    form.setAttribute("class", "expense-list_item");
+    form.innerHTML = ` 
+    <label>
+        <i class="fa-solid fa-bag-shopping"></i>
+    </label>
+    <input placeholder="Name of Expense" class="expense-name" maxlength="30" />
+    <input type="number" placeholder="Price" class="price" maxlength="10">
+    <button class="trash" onclick="remove()">
+        <i class="fa-solid fa-trash "></i>
+    </button>`
+    parentElement.appendChild(form);
+}
+
+function calculate() {
+    let monthlyIncome = parseInt(document.querySelector("#monthly-income").value);
+
+    const prices = document.querySelectorAll(".price");
+
+    let totalExpense = 0;
+
+    for (let price of prices) {
+        totalExpense += (+price.value);
+    }
+
+    const balance = (monthlyIncome - totalExpense).toLocaleString('en-US');
+
+
+    const BALANCE = document.querySelector("#total-balance");
+    const EXPENSES = document.querySelector("#total-expenses");
+    const INCOME = document.querySelector("#total-income");
+
+    BALANCE.textContent = `${balance}`;
+    EXPENSES.textContent = `${totalExpense.toLocaleString('en-US')}`;
+    INCOME.textContent = `${monthlyIncome.toLocaleString('en-US')}`;
+
+
+
+}
+
+function remove(element) {
+    element.closest(".expense-list_item").remove(); 
+}
